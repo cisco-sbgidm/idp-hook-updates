cb858155e2cf23bf2ae4cf5d595269dec0794d7c
jroschak <jroschak@cisco.com>
2020-02-10 15:06:40 +0200

    add newAdminApiName as argument

--- src/setupDuo/SetupDuo.ts
+++ src/setupDuo/SetupDuo.ts
@@ -3,12 +3,12 @@ import { DuoAdminAPI } from '../duo/DuoAdminAPI';
 import { DuoCreateAdminApiResponse } from './DuoCreateAdminApiResponse';
 
 const args = require('yargs')
-    .usage('Usage: $0 --ikey [string] --skey [string] --apihost [string]')
-    .demandOption(['ikey', 'skey', 'apihost'])
+    .usage('Usage: $0 --ikey [string] --skey [string] --apiHost [string] --adminApiName [string]')
+    .demandOption(['ikey', 'skey', 'apiHost', 'adminApiName'])
     .argv;
 
-const adminApi = new DuoCreateAdminApi(new DuoAdminAPI(args['ikey'], args['skey'], args['apihost']));
-adminApi.createIdpHookAdminAPI('testb').then((response: DuoCreateAdminApiResponse) => {
+const adminApi = new DuoCreateAdminApi(new DuoAdminAPI(args['ikey'], args['skey'], args['apiHost']));
+adminApi.createIdpHookAdminAPI(args['adminApiName']).then((response: DuoCreateAdminApiResponse) => {
   console.log(JSON.stringify(response));
 }).catch((error: any) => {
   console.log(JSON.stringify(error));
