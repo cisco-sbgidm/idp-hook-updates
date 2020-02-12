7483e7b80ec00587d2d6a871ca3db57b83748cb9
jroschak <jroschak@cisco.com>
2020-02-12 12:01:55 +0200

    move createAdminApi to duoAdminApi.ts

--- src/DuoAdminAPI.ts
+++ src/DuoAdminAPI.ts
@@ -1,13 +1,25 @@
 import * as _ from 'lodash';
 import crypto from 'crypto';
 import { URL } from 'url';
+import { Helper } from './Helper';
+import axios, { AxiosInstance } from 'axios';
 
 /**
  * Describes Duo Admin API
  */
 export class DuoAdminAPI {
 
-  constructor(readonly adminIkey: string, readonly adminSkey: string, readonly adminApiUrl: string) {}
+  private readonly axios: AxiosInstance;
+
+  constructor(readonly adminIkey: string, readonly adminSkey: string, readonly adminApiUrl: string) {
+    this.axios = axios.create({
+      baseURL: adminApiUrl,
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+  }
 
     /**
      * @param request
@@ -60,6 +72,41 @@ export class DuoAdminAPI {
         '\n',
     );
   }
+
+  createIdpHookAdminAPI(adminApiName: string): Promise<any> {
+    const requestBody = {
+      type: 'adminapi',
+      name: adminApiName,
+      adminapi_read_resource: 1,
+      adminapi_write_resource: 1,
+    };
+    const duoRequest = new DuoRequest('POST', '/admin/v1/integrations',
+                                      requestBody);
+    const duoAuthHeader = this.getSignedAuthHeader(
+        duoRequest);
+
+    return this.axios
+        .post<DuoCreateAdminApiResponse>(duoRequest.url, this.convertParams(requestBody), {
+          headers: {
+            Date: `${duoAuthHeader.date}`,
+            Authorization: `Basic ${duoAuthHeader.authorization}`,
+          },
+        })
+        .then((res: any) => {
+          return {
+            integrationKey: _.get(res, 'data.response.integration_key', ''),
+            secretKey: _.get(res, 'data.response.secret_key', ''),
+            name: _.get(res, 'data.response.name', ''),
+          };
+        }).catch(Helper.logError);
+  }
+}
+
+/**
+ * Describes create admin api response
+ */
+export class DuoCreateAdminApiResponse {
+  constructor(readonly ikey: string, readonly skey: string, readonly adminName: string) {}
 }
 
 /**
