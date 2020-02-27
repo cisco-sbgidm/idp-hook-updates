e5743f34ea017bfb7bcb839609f14d6e715f8a58
freylafert <olfreyla@cisco.com>
2020-02-27 11:38:22 +0200

    moving terraform generic parts out (#24)

---
+++ common/cloud/aws/terraform/modules/idp-hook-updates/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "is_okta" {
+  type = bool
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "lambda_file_path" {
+  type = string
+}
+
+variable "lambda_environment" {
+  type = map(string)
+}
+
+variable "lambda_handler" {
+  type = string
+}
+
+variable "lambda_timeout" {
+  type    = number
+  default = 3
+}
--- okta/duo/aws/terraform/main.tf
+++ okta/duo/aws/terraform/main.tf
@@ -15,12 +15,7 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix    = "${var.env}-${var.base_name}"
-  nodejs_runtime = "nodejs12.x"
-}
-
-data "aws_secretsmanager_secret" "idp_hook_updates" {
-  name = local.name_prefix
+  name_prefix = "${var.env}-${var.base_name}"
 }
 
 resource "aws_dynamodb_table" "events" {
@@ -45,28 +40,25 @@ resource "aws_dynamodb_table" "events" {
   }
 }
 
-resource "aws_iam_role" "idp_hook_updates" {
-  name = local.name_prefix
+module "idp-hook-updates" {
+  source           = "../../../../common/cloud/aws/terraform/modules/idp-hook-updates"
+  aws_region       = var.aws_region
+  name_prefix      = local.name_prefix
+  is_okta          = true
+  lambda_file_path = "${path.module}/../dist/idp-hook-updates.zip"
+  lambda_handler   = "./okta/duo/aws/src/OktaDuoAws.handler"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+  lambda_environment = {
+    OKTA_ENDPOINT     = var.okta_endpoint,
+    DUO_ENDPOINT      = var.duo_endpoint,
+    EVENTS_TABLE_NAME = aws_dynamodb_table.events.id,
+    SM_SECRETS_ID     = local.name_prefix
+  }
 }
 
-resource "aws_iam_role_policy" "idp_hook_updates" {
-  name   = local.name_prefix
-  role   = aws_iam_role.idp_hook_updates.id
+resource "aws_iam_role_policy" "idp_hook_updates_dynamodb" {
+  name   = "${local.name_prefix}-dynamodb"
+  role   = module.idp-hook-updates.role-id
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -80,162 +72,14 @@ resource "aws_iam_role_policy" "idp_hook_updates" {
       "Resource": [
         "${aws_dynamodb_table.events.arn}"
       ]
-    },
-    {
-        "Effect": "Allow",
-        "Action": "secretsmanager:GetSecretValue",
-        "Resource": "${data.aws_secretsmanager_secret.idp_hook_updates.arn}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-          "logs:CreateLogGroup"
-      ],
-      "Resource": [
-        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-      ],
-      "Resource": [
-        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.name_prefix}:*"
-      ]
     }
   ]
 }
 EOF
 }
 
-resource "aws_lambda_function" "idp_hook_updates" {
-  function_name    = local.name_prefix
-  handler          = "./okta/duo/aws/src/OktaDuoAws.handler"
-  role             = aws_iam_role.idp_hook_updates.arn
-  runtime          = local.nodejs_runtime
-  filename         = "${path.module}/../dist/idp-hook-updates.zip"
-  source_code_hash = filebase64sha256("${path.module}/../dist/idp-hook-updates.zip")
-
-  environment {
-    variables = {
-      OKTA_ENDPOINT     = var.okta_endpoint,
-      DUO_ENDPOINT      = var.duo_endpoint,
-      EVENTS_TABLE_NAME = aws_dynamodb_table.events.id,
-      SM_SECRETS_ID     = local.name_prefix
-    }
-  }
-}
-
-
-resource "aws_api_gateway_rest_api" "idp_hook_updates" {
-  name = local.name_prefix
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_method" "idp_hook_updates_get" {
-  rest_api_id   = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id   = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method   = "GET"
-  authorization = "NONE"
-  request_parameters = {
-    "method.request.header.X-Okta-Verification-Challenge" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "idp_hook_updates_get" {
-  rest_api_id             = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id             = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method             = aws_api_gateway_method.idp_hook_updates_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.idp_hook_updates.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "idp_hook_updates_get" {
-  depends_on = [aws_api_gateway_integration.idp_hook_updates_get]
-
-  rest_api_id = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method = aws_api_gateway_method.idp_hook_updates_get.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
-
-resource "aws_lambda_permission" "idp_hook_updates_get" {
-  statement_id  = "${local.name_prefix}-get-allow-execution-from-api-gateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.idp_hook_updates.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.idp_hook_updates.id}/*/${aws_api_gateway_method.idp_hook_updates_get.http_method}/"
-}
-
-resource "aws_api_gateway_method" "idp_hook_updates_post" {
-  rest_api_id   = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id   = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method   = "POST"
-  authorization = "NONE"
-  request_parameters = {
-    "method.request.header.Authorization" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "idp_hook_updates_post" {
-  rest_api_id             = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id             = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method             = aws_api_gateway_method.idp_hook_updates_post.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.idp_hook_updates.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "idp_hook_updates_post" {
-  rest_api_id = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method = aws_api_gateway_method.idp_hook_updates_post.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "idp_hook_updates_post" {
-  depends_on = [aws_api_gateway_integration.idp_hook_updates_post]
-
-  rest_api_id = aws_api_gateway_rest_api.idp_hook_updates.id
-  resource_id = aws_api_gateway_rest_api.idp_hook_updates.root_resource_id
-  http_method = aws_api_gateway_method.idp_hook_updates_post.http_method
-  status_code = "200"
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-resource "aws_lambda_permission" "idp_hook_updates_post" {
-  statement_id  = "${local.name_prefix}-post-allow-execution-from-api-gateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.idp_hook_updates.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.idp_hook_updates.id}/*/${aws_api_gateway_method.idp_hook_updates_post.http_method}/"
-}
-
-resource "aws_api_gateway_deployment" "idp_hook_updates" {
-  depends_on = [aws_api_gateway_integration.idp_hook_updates_get, aws_api_gateway_integration.idp_hook_updates_post]
-
-  rest_api_id = aws_api_gateway_rest_api.idp_hook_updates.id
-  stage_name  = var.base_name
-}
 
 # Data returned by this module.
 output "api-gateway-endpoint" {
-  value = aws_api_gateway_deployment.idp_hook_updates.invoke_url
+  value = module.idp-hook-updates.api-gateway-endpoint
 }
