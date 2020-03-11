8ac6d45934fd203c9174541240e091b4652ae83b
Oded Peer <opeer@cisco.com>
2020-03-11 11:37:00 +0200

    Add deployment for Okta/Duo on Azure

---
+++ okta/duo/azure/terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "azure_location" {
+  type = string
+}
+
+variable "base_name" {
+  type    = string
+  default = "okta-duo-idp-hook-updates"
+}
+
+variable "env" {
+  type = string
+}
+
+variable "okta_endpoint" {
+  type = string
+}
+
+variable "duo_endpoint" {
+  type = string
+}
