655c387a3f22386f767edbcf0fa1fa97b496d243
Oded Peer <opeer@cisco.com>
2020-03-17 10:06:38 +0200

    fix bug in parsing the userId, this worked when an admin changed the profile, but not if the user himself changed his own profile, now it works for both

--- okta/common/src/__mocks__/OktaService.ts
+++ okta/common/src/__mocks__/OktaService.ts
@@ -3,17 +3,12 @@
  */
 export class OktaService {
   async getUser(userId: string): Promise<any> {
-
     const oktaUserProfile = {
       email: 'test@test.com',
       firstName: 'first',
       lastName: 'last',
       middleName: 'middle',
     };
-    const mockOktaGetUser = jest.fn(() => Promise.resolve({
-      profile: oktaUserProfile,
-    }));
-
     return Promise.resolve({ profile: oktaUserProfile });
   }
 
