83fdea6fa623d88f3432986bc77c0a0874ab0e6f
opeer <opeer@cisco.com>
2020-05-25 15:04:11 +0300

    Handle Okta axios timeouts (#78)
    * Handle Okta axios timeouts
    
    * change logging
    
    * change logging

--- okta/duo/spec/src/client/okta-client.js
+++ okta/duo/spec/src/client/okta-client.js
@@ -12,7 +12,7 @@ class OktaClient {
       baseURL: oktaUrl,
       // `timeout` specifies the number of milliseconds before the request times out.
       // If the request takes longer than `timeout`, the request will be aborted.
-      timeout: 3000 // default is `0` (no timeout)
+      timeout: 5000 // default is `0` (no timeout)
     });
     this._axios.defaults.raxConfig = {
       instance: this._axios
@@ -36,20 +36,28 @@ class OktaClient {
     return this._request('delete', path);
   }
 
-  async _request(method, path, { qs, data } = {}) {
-    console.log(`Executing request to Okta ${method} ${path}`);
-    const resp = await this._axios.request({
-      method,
-      url: `${this._oktaUrl}${path}`,
-      headers: {
-        Authorization: `SSWS ${this._apiToken}`
-      },
-      params: qs,
-      data
-    });
-    return resp.data;
+  _request(method, path, { qs, data } = {}, iteration = 1) {
+    if (iteration > 3) {
+      return Promise.reject(new Error(`Too many retries ${iteration}`));
+    }
+    console.log(`Executing request to Okta ${method} ${path}, iteration ${iteration}`);
+    return this._axios
+      .request({
+        method,
+        url: `${this._oktaUrl}${path}`,
+        headers: {
+          Authorization: `SSWS ${this._apiToken}`
+        },
+        params: qs,
+        data
+      })
+      .then(res => res.data)
+      .catch(error => {
+        console.log(`timed out, retrying ${JSON.stringify(error)}`);
+        const nextIteration = iteration + 1;
+        return this._request(method, path, { qs, data }, nextIteration);
+      });
   }
-
 }
 
 module.exports = OktaClient;
