0c531ae63af374c1e3bb96ef00568f2bd334904e
Scott Burnette (scburnet) <scburnet@cisco.com>
2021-11-02 09:03:56 -0700

    Add logs to kibana

--- common/cloud/aws/terraform/modules/idp-hook-updates/idp-hook-updates.tf
+++ common/cloud/aws/terraform/modules/idp-hook-updates/idp-hook-updates.tf
@@ -83,6 +83,13 @@ resource "aws_cloudwatch_log_group" "log_group" {
   retention_in_days = 30
 }
 
+resource "aws_cloudwatch_log_subscription_filter" "subscription_filter" {
+  name            = "${aws_lambda_function.idp_hook_updates.function_name}-logs-to-elasticsearch"
+  log_group_name  = aws_cloudwatch_log_group.log_group.name
+  filter_pattern  = ""
+  destination_arn = data.aws_ssm_parameter.cwl_stream_lambda_arn.value
+}
+
 resource "aws_api_gateway_rest_api" "idp_hook_updates" {
   name = var.name_prefix
   endpoint_configuration {
