9360cd17e7d5617e39851b132672e4421854933e
Scott Burnette (scburnet) <scburnet@cisco.com>
2021-12-06 08:30:37 -0500

    Add console log for debugging

--- okta/duo/spec/src/__tests__/ci.spec.ts
+++ okta/duo/spec/src/__tests__/ci.spec.ts
@@ -45,6 +45,7 @@ function pollDuoUntilMatch(method: string, path: string, params: any, field: str
               cb(false);
             } else {
               const duoVal = _.get(duoResponse, field);
+              console.log(JSON.stringify(duoResponse));
               cb(duoVal === expectedValue, duoVal);
             }
           });
